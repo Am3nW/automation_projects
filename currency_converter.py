a2364bf822c9ee70f2266320e684915f07c43866
Am3nW <137397167+Am3nW@users.noreply.github.com>
2024-05-02 11:59:53 +0300

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -1,1 +1,26 @@
+import requests
+api_key = "fca_live_ywXQP99lIaQ9bNcqp17E9oQwqF1ATav3FpCtDIz9"
+base_url = f'https://api.freecurrencyapi.com/v1/latest?apikey={api_key}'
 
+Currencies = ["USD","CAD","EUR","AUD","CNY"]
+
+def convert_currency(base):
+    currencies = ",".join(Currencies)
+    url = f"{base_url}&base_currency={base}&currencies={currencies}"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data["data"]
+    except:
+        print("Invalid Currency")
+        return None
+while True:
+    base = (input("Enter the currency: ")).upper()
+    if base == "Q":
+        break
+    data = convert_currency(base)
+    if not data:
+        continue  
+    del data[base]
+    for curr, value in data.items():
+        print(f"{curr}: {value}")
